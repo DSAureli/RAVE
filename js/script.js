859273e66e898981420b918b3bbc3a308da72a93
DSAureli <ds.aureli@gmail.com>
2017-06-08 13:29:21 +0200

    Revert "now queries are binded o searchbar and the crossref links are opened in a new tab."
    This reverts commit ba6782aef08f18e41f18134eca076bac3f8c3d0e.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ String.prototype.format = function()
 	return content;
 };
 
-function updateContent(index, title, sfor)
+function updateContent(index, title)
 {
 	$("#column_wiki").dimmer("show");
 	$("#column_wiki .loader").removeClass("disabled");
@@ -23,7 +23,7 @@ function updateContent(index, title, sfor)
 		data:
 		{
 			action: "parse",
-			page: sfor,
+			page: "Steins;Gate (anime)",
 			section: index,
 			prop: "text",
 			format: "json"
@@ -60,7 +60,7 @@ function updateContent(index, title, sfor)
 	/* CROSSREF */
 	$.ajax(
 	{
-		url: "https://api.crossref.org/works?query=" + sfor + "+" + title.split(" ").join("+") + "&rows=10&sort=score",
+		url: "https://api.crossref.org/works?query=steins+gate+" + title.split(" ").join("+") + "&rows=10&sort=score",
 		dataType: "text",
 		success: function(cross)
 		{
@@ -71,7 +71,7 @@ function updateContent(index, title, sfor)
 			{
 				if (item.issue != "0")
 				{
-					$("#content_crossref").append("<div class='ui segment'><p>Title: {0}</br>Publisher: {1}</br> Type: {2}</br> <a href='{3}' target='_blank'>Get this content</a></p></div>".format(item.title[0], item.publisher, item.type, item.URL));
+					$("#content_crossref").append("<div class='ui segment'><p>Title: {0}</br>Publisher: {1}</br> Type: {2}</br> <a href='{3}'>Get this content</a></p></div>".format(item.title[0], item.publisher, item.type, item.URL));
 				}
 			});
 		},
@@ -96,49 +96,30 @@ $(document).ready(function()
 	{
 		onChange: updateContent
 	});
-    
-    var searchSec;
-    var searchFor;
-
-    $('#sbar').on('keypress', function(e){
-        
-        if(e.which == 13)
-        {
-            
-            /*$("#dropdown_sections").dropdown('clear');*/
-            
-            searchFor = $(this).val();
-            $('#mainTitle').text(searchFor);
-            $.ajax(
-            {
-                url: "http://en.wikipedia.org/w/api.php",
-                data:
-                {
-                    action: "parse",
-                    prop: "sections",
-                    page: searchFor,
-                    format: "json"
-                },
-                dataType: "jsonp",
-                success: function(apiResult)
-                {
-                    searchSec = apiResult.parse.sections[0];
-                    $.each(apiResult.parse.sections, (i, section) =>
-                    {
-                        if (section.line != "References" && section.line != "External links")
-                            $("#menu_sections").append('<div class="item" data-value="{0}">{1}</div>'.format(section.index, section.line));
-                    });
-                },
-                error: outputError()
-            });
-
-            updateContent(0, searchSec, searchFor);
-        }
-    });
-    
-    
-    
-        
+	
+	$.ajax(
+	{
+		url: "http://en.wikipedia.org/w/api.php",
+		data:
+		{
+			action: "parse",
+			prop: "sections",
+			page: "Steins;Gate (anime)",
+			format: "json"
+		},
+		dataType: "jsonp",
+		success: function(apiResult)
+		{
+			$.each(apiResult.parse.sections, (i, section) =>
+			{
+				if (section.line != "References" && section.line != "External links")
+					$("#menu_sections").append('<div class="item" data-value="{0}">{1}</div>'.format(section.index, section.line));
+			});
+		},
+		error: outputError()
+	});
+	
+	updateContent(0, "Summary");
 });
 
 function outputError(errorMessage)
